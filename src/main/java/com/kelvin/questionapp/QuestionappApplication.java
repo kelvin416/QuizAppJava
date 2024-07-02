c63eb95828f3b4b287788c8bfc7aced2bf95d369
Kelvin Rotich <kelvinrotich416@gmail.com>
2024-07-02 19:19:44 +0300

    gradle setup

---
+++ src/main/java/com/kelvin/questionapp/QuestionappApplication.java
@@ -0,0 +1,13 @@
+package com.kelvin.questionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionappApplication.class, args);
+	}
+
+}
